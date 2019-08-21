9b99520a31daabca4d9ef2f577ed5376b98b08b8
mj08186 <mj08186@CEIT2553214X015.ad.georgiasouthern.edu>
2019-08-21 16:50:05 -0400

    Ask for last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,13 +1,23 @@
 package assignment;
+import java.util.Scanner;
 
 public class FirstClassInGit {
+	
 
 	public static void main(String[] args) {
-		System.out.println("Hello my name is Michael");
+		//Create a scanner
+		Scanner input = new Scanner(System.in);
 		
 		//add a statement to display your name in the console
+		System.out.println("Hello my name is Michael");
 		
-		//Then commit your changes
+		//Request the user's name
+		System.out.print("Please enter your last name: ");
+		String userName = input.next();
+		
+		//Display the user's name
+		System.out.print("Hello ");
+		System.out.println(userName);
 	}
 
 }
